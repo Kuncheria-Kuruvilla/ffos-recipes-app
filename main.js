7674071d7e82333b4c6daaae3e81bf102c313fa1
Rejah Rehim <rejah@digitalbrandgroup.com>
2014-09-13 01:29:06 +0530

    Cleaned Js

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ var recipeApp = {
                         result += "<div class='teamalign'><img class='team-thumb img-circle' src="+obj.recipes[prop].image_url+"> </div>";
                         result += "<h3>"+obj.recipes[prop].title+"</h3>";
                         result += "<div class='job-position'>#12334556</div>";
-                        result += "<a href='http://food2fork.com/api/get?key="+appKey+"&rId="+obj.recipes[prop].recipe_id+"' title='More Info' class='link-more-info' onclick='alert(\"hii\")'>More Info...</a>";
+                        result += "<a href='http://food2fork.com/api/get?key="+appKey+"&rId="+obj.recipes[prop].recipe_id+"' title='More Info' class='link-more-info' onclick='alert("")'>More Info...</a>";
                         result += "</div>";
                     }
                 }
@@ -36,35 +36,6 @@ var recipeApp = {
                 if('vibrate' in navigator) {
                     // ... vibrate for a second
                     navigator.vibrate(1000);
-                }
-            }
-        }
-        request.send();
-    },
-    QueryRecipieDetails: function (rId) {
-        var request = new XMLHttpRequest({ mozSystem: true });
-        request.open("GET", "http://food2fork.com/api/get?key="+appKey+"&rId="+rId);
-        // when the Ajax request resolves, load content into a <script> tag
-        request.onreadystatechange = function() {
-            if (request.readyState == 4) {
-                obj = JSON.parse(request.responseText);
-                result = "<div>";
-                for (var prop in obj.recipes) {
-                    if (obj.recipes.hasOwnProperty(prop)) {
-                      // or if (Object.prototype.hasOwnProperty.call(obj,prop)) for safety...
-                        //console.log("prop: " + prop + " value: " + obj.recipes[prop].recipe_id)
-                        result += "<li>";
-                        result += "<button id='recipes"+obj.recipes[prop]+"'"+obj.recipes[prop].recipe_id+"</button>";
-                        result += "Recipe Name: " + obj.recipes[prop].title;
-                        result += "Recipe Image Url " + obj.recipes[prop].image_url;
-                        result += "</li>";
-                    }
-                }
-                result += "</div>";
-                if (obj.recipes.length) {
-                    serchResult.innerHTML = result;
-                } else{
-                    serchResult.innerHTML = "No result Found";
                 }
             }
         }
